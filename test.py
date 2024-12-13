9b181f066ca4e562e76e9eb29fe1141d9f151630
Giles Synaeve <145968081+GilesS420@users.noreply.github.com>
2024-12-13 09:58:47 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+getal1 = 50
+getal2 =40
+what = "WTF just happend"
+
+if(getal1 < getal2):
+    print("help")
+
+elif(getal2< getal1):
+    print("Yippee")
+
+else:
+    print(what)
+
+print(180 / )
